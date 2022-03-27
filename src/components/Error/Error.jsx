38628f9e6bbdac66d1e7579d390845022cc2a887
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-27 12:55:35 -0400

    fix error component, import style

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Error.module.css";
 // import propTypes from "prop-types";
 
 function Error({ message }) {
   return (
-    <div className={`{styles.error} alert position-absolute`} role="alert">
+    <div className={`${styles.error} alert position-absolute`} role="alert">
       {message}
     </div>
   );
