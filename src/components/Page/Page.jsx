89f0c213bab112784061682d9925685271d8760d
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-26 20:53:31 -0400

    import components into Page

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,16 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import styles from "./Page.module.css";
+import Form from "../Form/Form";
+import Loader from "../Loader/Loader";
+import Error from "../Error/Error";
+import Forecast from "../Forecast/Forecast";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 
 const Page = () => {
-    return (
-        <Fragment>
-            <Header />
-            <div className={styles.box}>HII</div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Header />
+      <div className={styles.box}>
+        <Form />
+        <Error />
+        <Loader />
+        <Forecast />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Page;
-
