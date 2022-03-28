f65d5a473ab7f23f8fb2c22c3d4940f81acea5ba
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-28 19:58:00 -0400

    fix small errors such as displaying forecast for a day and for the whole week

--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -1,39 +1,40 @@
-import React from "react";
-import locationIcon from "./assets/location-pin.png";
-import styles from "./CurrentDay.module.css";
-import PropTypes from "prop-types";
+import React from 'react';
+import locationIcon from './assets/location-pin.png';
+import styles from './CurrentDay.module.css';
+import PropTypes from 'prop-types';
 
 const CurrentDay = ({ weekday, date, location, temperature, weatherIcon, weatherDescription }) => (
-  <div className="d-flex">
-      <div className={styles.img}></div>
-      <div className={styles.gradient}></div>
-      <div className={`${styles.cardInner} d-flex flex-column justify-content-between pt-3 pb-2 pl-2`}>
-          <div>
-              <h2 className="font-weight-bold mb-1">{weekday}</h2>
-              <p className="mb-0">{date}</p>
-              <p className="d-flex align-items-baseline font-weight-lighter mb-1">
-                  <img width="10" height="15" src={locationIcon} className="mr-1" alt="location pin icon" />
-                  <span>{location}</span>
-              </p>
-          </div>
-          <div>
-              <img width="45" src={weatherIcon} alt="" />
-              <h2 className="font-weight-bold mb-1">
-                  <span>{temperature}</span>°C
-              </h2>
-              <h5 className="font-weight-lighter">{weatherDescription}</h5>
-          </div>
-      </div>
-  </div>
+    <div className={styles.container}>
+        <div className={styles.img}></div>
+        <div className={styles.gradient}></div>
+
+        <div className={styles.cardInner}>
+            <div>
+                <h2 className="font-weight-bold mb-3">{weekday}</h2>
+                <p className={styles.date}>{date}</p>
+                <p className={styles.main}>
+                    <img  className="m-2" width="15" height="20" src={locationIcon} alt="location pin icon" />
+                    <span className={styles.cityLocation}>{location}</span>
+                </p>
+            </div>
+            <div className={styles.dayForecast}>
+                <img width="45" src={weatherIcon} alt="" />
+                <h2 className="font-weight-bold mb-2">
+                    <span>{temperature}</span>°C
+                </h2>
+                <h5>{weatherDescription}</h5>
+            </div>
+        </div>
+    </div>
 );
 
 CurrentDay.propTypes = {
-  weekday: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  weatherIcon: PropTypes.string.isRequired,
-  temperature: PropTypes.number.isRequired,
-  weatherDescription: PropTypes.string.isRequired,
+    weekday: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    weatherIcon: PropTypes.string.isRequired,
+    temperature: PropTypes.number.isRequired,
+    weatherDescription: PropTypes.string.isRequired,
 };
 
 export default CurrentDay;
--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,39 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-import Header from "../Header/Header";
-import Form from "../Form/Form";
-import Error from "../Error/Error";
-import Loader from "../Loader/Loader";
-import Forecast from "../Forecast/Forecast";
+import Header from '../Header/Header';
+import Form from '../Form/Form';
+import Error from '../Error/Error';
+import Loader from '../Loader/Loader';
+import Forecast from '../Forecast/Forecast';
 
-import useForecast from "../../hooks/useForecast";
+import useForecast from '../../hooks/useForecast';
 
-import styles from "./Page.module.css";
+import styles from './Page.module.css';
 
 const Page = () => {
-  const { isError, isLoading, forecast, submitRequest } = useForecast();
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
 
-  const onSubmit = (value) => {
-    submitRequest(value);
-  };
+    const onSubmit = value => {
+        submitRequest(value);
+    };
 
-  return (
-    <Fragment>
-      <Header />
-      {!forecast && (
-        <div className={`${styles.box} position-relative`}>
-          {/* Form */}
-          {!isLoading && <Form submitSearch={onSubmit} />}
-          {/* Error */}
-          {isError && <Error message={isError} />}
-          {/* Loader */}
-          {isLoading && <Loader />}
-        </div>
-      )}
-      {/* Forecast */}
-      {forecast && <Forecast forecast={forecast} />}
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Header />
+            {!forecast ? (
+                <div className={`${styles.box} position-relative`}>
+                    {/* Form */}
+                    {!isLoading && <Form submitSearch={onSubmit} />}
+                    {/* Error */}
+                    {isError && <Error message={isError} />}
+                    {/* Loader */}
+                    {isLoading && <Loader />}
+                </div>
+            ) : (
+                <Forecast forecast={forecast} />
+            )}
+        </Fragment>
+    );
 };
 
 export default Page;
